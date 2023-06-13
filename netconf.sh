c205efa0a3605e7c6f8c8a3de5c1df9b92205008
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-13 19:45:30 +0300

    Update netconf.sh

--- netconf.sh
+++ netconf.sh
@@ -8,12 +8,12 @@ read -p "IP-address/subnet mask: " netconfIpMask
 read -p "Gateway: " netconfGateway
 read -p "DNS Servers: " netconfDns
 
-sed -i -e 's/dhcp4: yes/dhcp4: no/' /etc/netplan/01-netcfg.yaml
+sed -i -e 's/dhcp4: yes/dhcp4: no/' 01-netcfg.yaml
 
-sed -i -e "8a|      addresses: [$netconfIpMask]|g"  /etc/netplan/01-netcfg.yaml
-sed -i -e "9a|      gateway4: $netconfGateway|g"  /etc/netplan/01-netcfg.yaml
-sed -i -e "10a|      nameservers:"  /etc/netplan/01-netcfg.yaml
-sed -i -e "11a|        addresses: [$netconfDns]|g"  /etc/netplan/01-netcfg.yaml
+sed -i -e "8a\      addresses: [$netconfIpMask]"  01-netcfg.yaml
+sed -i -e "9a\      gateway4: $netconfGateway"  01-netcfg.yaml
+sed -i -e "10a\      nameservers:"  01-netcfg.yaml
+sed -i -e "11a\        addresses: [$netconfDns]"  01-netcfg.yaml
 
 netplan --debug generate
 netplan --debug apply
