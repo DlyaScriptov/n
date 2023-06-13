f0446050ba108492fbacf4eb6301676365ebdd5a
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-13 19:15:47 +0300

    Create netconf.sh

---
+++ netconf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+netconfIpMask=A
+netconfGateway=A
+netconfDns=A
+
+read -p "IP-address/subnet mask: " netconfIpMask
+read -p "Gateway: " netconfGateway
+read -p "DNS Servers: " netconfDns
+
+sed -i -e 's/dhcp4: yes/dhcp4: no/' /etc/netplan/01-netcfg.yaml
+
+sed -i -e "8a|      addresses: [$netconfIpMask]|g"  /etc/netplan/01-netcfg.yaml
+sed -i -e "9a|      gateway4: $netconfGateway|g"  /etc/netplan/01-netcfg.yaml
+sed -i -e "10a|      nameservers:"  /etc/netplan/01-netcfg.yaml
+sed -i -e "11a|        addresses: [$netconfDns]|g"  /etc/netplan/01-netcfg.yaml
+
+netplan --debug generate
+netplan --debug apply
+
+apt-get -y -q install openssh-server
+
+systemctl start sshd
+
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow OpenSSH
+ufw allow ssh
+ufw allow 22
+ufw enable
+
+reboot
