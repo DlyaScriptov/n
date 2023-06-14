835df3a00e42f8d30b05390f89714b8d80547cbd
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-14 18:46:48 +0300

    Update nbics_install.sh

--- nbics_install.sh
+++ nbics_install.sh
@@ -1,1 +1,31 @@
+nbicsNameDomain=A
+nbicsNameDataBase=A
+nbicsPasswordDataBase=A
+
+pwdScan=$(pwd)
+
+read -p "Введите имя домена для NBICS: " nbicsNameDomain
+read -p "Введите имя базы данных: " nbicsNameDataBase
+read -p "Введите пароль администратора базы данных: " nbicsPasswordDataBase
+
+apt-get -y -q install curl
+apt-get -y -q install apt-transport-https
+apt-get -y -q install ufw
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 8080/tcp
+sudo ufw allow 8443/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 3478
+sudo ufw allow 40000:57000/tcp
+sudo ufw allow 40000:57000/udp
+sudo ufw allow 57001:65535/tcp
+sudo ufw allow 57001:65535/udp
+sudo ufw allow 1433
+sudo ufw allow from 127.0.0.1 to any port 1433
+sudo ufw enable
+
+
 
