48d1940df42af9867154939c664293ab01baf820
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-14 20:39:26 +0300

    Update nbics_install.sh

--- nbics_install.sh
+++ nbics_install.sh
@@ -13,21 +13,26 @@ apt-get -y -q install curl
 apt-get -y -q install apt-transport-https
 apt-get -y -q install ufw
 
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow 8080/tcp
-sudo ufw allow 8443/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 3478
-sudo ufw allow 40000:57000/tcp
-sudo ufw allow 40000:57000/udp
-sudo ufw allow 57001:65535/tcp
-sudo ufw allow 57001:65535/udp
-sudo ufw allow 1433
-sudo ufw allow from 127.0.0.1 to any port 1433
-sudo ufw enable
-sudo ufw delete allow 1433
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 8080/tcp
+ufw allow 8443/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 3478
+ufw allow 40000:57000/tcp
+ufw allow 40000:57000/udp
+ufw allow 57001:65535/tcp
+ufw allow 57001:65535/udp
+ufw allow 1433
+ufw allow from 127.0.0.1 to any port 1433
+ufw enable
+ufw delete allow 1433
+
+FILE=/home/download
+if [ ! -d "$FILE" ]; then
+    mkdir /home/download
+fi
 
 
 
