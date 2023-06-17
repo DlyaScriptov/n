949ad336f01546eef1c2532c3849a4cbf61f2df6
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-17 15:56:11 +0300

    Update mssql_install.sh

--- files/mssql_install.sh
+++ files/mssql_install.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Use the following variables to control your install:
 
-# Password for the sa user (required)
+# Password for the SA user (required)
 MSSQL_SA_PASSWORD='<YourStrong!Passw0rd>'
 
 # Product ID of the version of SQL server you're installing
 # Must be evaluation, developer, express, web, standard, enterprise, or your 25 digit product key
 # Defaults to developer
-MSSQL_PID='express'
+MSSQL_PID='evaluation'
 
 # Enable SQL Server Agent (recommended)
 SQL_ENABLE_AGENT='y'
@@ -27,25 +27,25 @@ then
 fi
 
 echo Adding Microsoft repositories...
-curl https://packages.microsoft.com/keys/microsoft.asc |  apt-key add -
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 repoargs="$(curl https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2022.list)"
-add-apt-repository "${repoargs}"
+sudo add-apt-repository "${repoargs}"
 repoargs="$(curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
-add-apt-repository "${repoargs}"
+sudo add-apt-repository "${repoargs}"
 
 echo Running apt-get update -y...
-apt-get update -y
+sudo apt-get update -y
 
 echo Installing SQL Server...
-apt-get install -y mssql-server
+sudo apt-get install -y mssql-server
 
 echo Running mssql-conf setup...
-MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD \
+sudo MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD \
      MSSQL_PID=$MSSQL_PID \
      /opt/mssql/bin/mssql-conf -n setup accept-eula
 
 echo Installing mssql-tools and unixODBC developer...
-ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
 
 # Add SQL Server tools to the path by default:
 echo Adding SQL Server tools to your path...
@@ -57,29 +57,29 @@ source ~/.bashrc
 if [ ! -z $SQL_ENABLE_AGENT ]
 then
   echo Enabling SQL Server Agent...
-  /opt/mssql/bin/mssql-conf set sqlagent.enabled true
+  sudo /opt/mssql/bin/mssql-conf set sqlagent.enabled true
 fi
 
 # Optional SQL Server Full Text Search installation:
 if [ ! -z $SQL_INSTALL_FULLTEXT ]
 then
     echo Installing SQL Server Full-Text Search...
-    apt-get install -y mssql-server-fts
+    sudo apt-get install -y mssql-server-fts
 fi
 
 # Configure firewall to allow TCP port 1433:
 echo Configuring UFW to allow traffic on port 1433...
-ufw allow 1433/tcp
-ufw reload
+sudo ufw allow 1433/tcp
+sudo ufw reload
 
 # Optional example of post-installation configuration.
 # Trace flags 1204 and 1222 are for deadlock tracing.
 # echo Setting trace flags...
-# /opt/mssql/bin/mssql-conf traceflag 1204 1222 on
+# sudo /opt/mssql/bin/mssql-conf traceflag 1204 1222 on
 
 # Restart SQL Server after installing:
 echo Restarting SQL Server...
-systemctl restart mssql-server
+sudo systemctl restart mssql-server
 
 # Connect to server and get the version:
 counter=1
@@ -90,7 +90,7 @@ do
   sleep 3s
   /opt/mssql-tools/bin/sqlcmd \
     -S localhost \
-    -U sa \
+    -U SA \
     -P $MSSQL_SA_PASSWORD \
     -Q "SELECT @@VERSION" 2>/dev/null
   errstatus=$?
@@ -110,7 +110,7 @@ then
   echo Creating user $SQL_INSTALL_USER
   /opt/mssql-tools/bin/sqlcmd \
     -S localhost \
-    -U sa \
+    -U SA \
     -P $MSSQL_SA_PASSWORD \
     -Q "CREATE LOGIN [$SQL_INSTALL_USER] WITH PASSWORD=N'$SQL_INSTALL_USER_PASSWORD', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=ON, CHECK_POLICY=ON; ALTER SERVER ROLE [sysadmin] ADD MEMBER [$SQL_INSTALL_USER]"
 fi
