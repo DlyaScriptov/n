9dad5a18d8de0b48b31cdb800d1078765916f9f2
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-18 19:19:03 +0300

    Update 3_port_conf.sh

--- 3_port_conf.sh
+++ 3_port_conf.sh
@@ -11,6 +11,7 @@ ufw allow 80/tcp
 ufw allow 443/tcp
 ufw allow 10000/udp
 ufw allow 5349/tcp
+
 ufw allow 8080/tcp
 ufw allow 8443/tcp
 ufw allow 3478
@@ -28,6 +29,14 @@ ufw status numbered
 iptables -I INPUT -p tcp --match multiport --dports 80,443 -j ACCEPT
 iptables -I INPUT -p udp --dport 10000 -j ACCEPT
 iptables -I INPUT -p tcp --dport 5349 -j ACCEPT
+
+iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8443 -j ACCEPT
+iptables -I INPUT -p tcp --dport 3478 -j ACCEPT
+iptables -I INPUT -p tcp --dport 40000:57000 -j ACCEPT
+iptables -I INPUT -p tcp --dport 57001:65535 -j ACCEPT
+iptables -I INPUT -p udp --dport 57001:65535 -j ACCEPT
+iptables -I INPUT -p tcp --dport 1433 -j ACCEPT
 
 DEBIAN_FRONTEND=noninteractive apt-get  -y -q install iptables-persistent
 netfilter-persistent save
